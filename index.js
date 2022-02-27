10c840ec247cfdcc432a544b3d6c34f30ef65f4e
shemy <shemysb@gmail.com>
2022-02-27 22:11:39 +0530

    sss

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const authRoute = require("./src/routes/Authoriztion");
 const catRoute = require("./src/routes/Categories");
 const postRoute = require("./src/routes/Posts");
 
-
-const PORT='5000';
+var port = process.env.PORT || 5000;
 
 const app=express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -52,6 +50,4 @@ app.get('/*',function(req,res){
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-
-var port = process.env.PORT || PORT;
 app.listen(port,()=>console.log(`Server is running at PORT NO: ${port}`));
